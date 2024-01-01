78c68f722de46ad5e790994d3e241cb21c1628d3
mari2t <subcodeoft@gmail.com>
2024-01-01 10:43:35 +0900

    style : tailwind

--- blog_client/src/app/page.tsx
+++ blog_client/src/app/page.tsx
@@ -1,14 +1,8 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
-
-interface Post {
-  id: number;
-  title: string;
-  content: string;
-  created_at: Date;
-  updated_at: Date;
-}
+import { Post } from "@/types";
+import Link from "next/link";
 
 export default function Home() {
   const [posts, setPosts] = useState<Post[]>([]);
@@ -30,12 +24,43 @@ export default function Home() {
     fetchPosts();
   }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {posts.map((post) => (
-        <div key={post.id}>
-          <p>{post.title}</p>
+    <div className="bg-gray-100 p-8 min-h-screen">
+      <h2 className="text-4xl font-bold text-center text-gray-800 mb-8">
+        Rails & Next.js Blog
+      </h2>
+      <div className="text-center mb-8">
+        <Link
+          href="/create-post"
+          className="inline-block bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition-colors"
+        >
+          Create new Post
+        </Link>
+      </div>
+      <div className="flex justify-center items-center">
+        {" "}
+        {/* Flexboxを適用 */}
+        <div className="space-y-8 w-2/3">
+          {posts.map((post) => (
+            <div key={post.id} className="bg-white shadow-lg rounded-lg p-6">
+              <Link
+                href={`post/${post.id}`}
+                className="text-2xl font-semibold hover:text-blue-500 transition-colors"
+              >
+                {post.title}
+              </Link>
+              <p className="text-gray-600 mt-2">{post.content}</p>
+              <div className="flex justify-between mt-4">
+                <button className="bg-yellow-500 text-white px-4 py-1 rounded hover:bg-yellow-600 transition-colors">
+                  Edit
+                </button>
+                <button className="bg-red-500 text-white px-4 py-1 rounded hover:bg-red-600 transition-colors">
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
-    </main>
+      </div>
+    </div>
   );
 }
---
+++ blog_client/src/types.ts
@@ -0,0 +1,7 @@
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  created_at: Date;
+  updated_at: Date;
+}
